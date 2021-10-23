2d03557e5a350dd8418046cf8432386cc4e438df
saumya-2259 <saumyas.20cse@kongu.edu>
2021-10-23 11:35:00 +0530

    placement commit

---
+++ placedetail.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char name[50]="saumya",email[100]="saumyasakthivel@gmail.com",edu_email[100]="saumyas.20cse@kongu.edu",
+	     int rollno=78,phno=65422,no_of_arrears=0;
+	float GPA_1=9.93,GPA_2=9.43,GPA_3=8.88,CGPA=8.98;
+	printf("name:%c",name);
+	printf("roll no:%d",rollno);
+	 printf("mobile no :%d",phno);
+	 printf("personal email:%c",email);
+	 printf("edu email:%c",edu_email);
+         printf("number of arreares:%d",no_of_arrear);
+	 printf("GPA 1:%f",GPA_1);
+	 printf("GPA 2:%f",GPA_2);
+	 printf("GPA 3:%f",GPA_3);
+	 printf("CGPA :%f",CGPA);}
+
