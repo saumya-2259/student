35bd03deca43a342abfe810edbaa023f2755223b
saumya-2259 <saumyas.20cse@kongu.edu>
2021-10-23 11:18:20 +0530

    insurance commit

---
+++ insdetails.c
@@ -0,0 +1,23 @@
+#include<stdio.>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+	int nomin_age=35,nomin_phno=6773,nomin_aadharno=435774211;
+	char name[50]="saumya",nominee_name[50]="swathi",nomin_relation[50]="sister",
+	     nomin_occupation[100]="business",nomin_email[100]="swathi777@gmail.com";
+	printf("name:%c",name);
+	printf("Nominee name :%c",nominee_name);
+	 printf("Relationship of Nominee :%c",nomin_relation);
+	  printf("Nominee age :%d",nomin_age);
+	   printf("Nominee phone no :%d",nomin_phno);
+	    printf("Nominee occupation :%c",nomin_occpation);
+	     printf("Nominee email :%c",nomin_email);
+	      printf("Nominee aadhar no :%c",nomin_aadharno);
+}
+
+
+
+
+
+
