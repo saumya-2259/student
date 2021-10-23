f9db95f72aef79db7aebf7e1b7dfc646e8e34046
saumya-2259 <saumyas.20cse@kongu.edu>
2021-10-23 10:59:30 +0530

    first commit

---
+++ details.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	char name[50]="saumya",father_name[100]="sakthivel",mother_name[100]="manjula";
+	char email[100]="saumyasakthivel@gmail.com",blood_group[50]="O+ve",address[50]="karur";
+	int rollno=234,age=25,phno=9878,aadhar_no=6774;
+	printf("NAME:%c",name);
+	printf("ROLL NO : %d",rollno);
+	printf("FATHER NAME: %c",father_name);
+	printf("MOTHER NAME:%c",mother_name);
+	printf("AGE:%d",age);
+	printf("MOBILE NUMBER:%d",phno);
+	printf("Email:%c",email);
+	printf("BLOOD GROUP:%c",blood_group);
+	printf("ADDRESS:%c",address);
+	printf("AADHAR no:%d",aadhar_no);}
+
+
+
